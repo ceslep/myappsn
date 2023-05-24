630a32d0c09fb2fb696fde968e83208f6d29253a
ceslep <ceslep@gmail.com>
2023-05-24 14:55:11 -0500

    list

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -3,22 +3,23 @@
   import { onDestroy, onMount } from "svelte";
   import * as geolocation from "@nativescript/geolocation";
   import { CoreTypes } from "@nativescript/core";
+
   CoreTypes.Accuracy;
 
   let _latitude: string = "";
   let _longitude: string = "";
-  let _altitude:string="";
-  let _speed:string="";
-  let message="Cesar";
+  let _altitude: string = "";
+  let _speed: string = "";
+  let message = "Cesar";
 
-
-  const getLocalization = async()=>{
+  const getLocalization = async () => {
     geolocation.enableLocationRequest().then(() => {
       geolocation
         .getCurrentLocation({
           desiredAccuracy: CoreTypes.Accuracy.high,
           maximumAge: 5000,
           timeout: 20000,
+          updateTime: 1000,
         })
         .then((currentLocation) => {
           currentLocation;
@@ -27,50 +28,72 @@
           _altitude = currentLocation.altitude.toString();
           _speed = currentLocation.speed.toString();
           console.log("My current latitude: ", currentLocation);
-          
         });
     });
-  }
-  let iniciarlInterval=false;
+  };
+  let iniciarlInterval = false;
 
+  let _location;
   onMount(async () => {
     getLocalization();
-    iniciarlInterval=true;
+   // iniciarlInterval = true;
   });
 
-  let interval:any;
+  let interval: any;
 
-  $:if(iniciarlInterval){
-    interval=setInterval(()=>{
-        getLocalization();
-    },5000)
+  $: if (iniciarlInterval) {
+    interval = setInterval(() => {
+      getLocalization();
+    }, 55000);
   }
 
-  onDestroy(()=>{
+  onDestroy(() => {
     clearInterval(interval);
-  })
+  });
 
-  let nombre = '';
+  let nombre = "";
 
   function handleTextChange() {
-    console.log('Nuevo valor:', nombre);
+  //  console.log("Nuevo valor:", nombre);
   }
+
+  let data: any;
+
+  const getItems = async () => {
+    try{
+    console.log("Consultando datos");
+    const response = await fetch(
+      "https://app.iedeoccidente.com/getasignacion.php"
+    );
+    const data=await response.json();
+    console.log("datos Consultados")
+    return data;
+    }catch(error){
+      console.log(error)
+    }
+  };
 </script>
 
 <page>
-  <actionBar title="Localización" backgroundColor="navy" color="white"/>
-  
+  <actionBar title="Localización" backgroundColor="navy" color="white" />
+
   <flexboxLayout flexDirection="column" backgroundColor="#FFFFDD">
-    <textField hint="" bind:text="{_latitude}" on:textChange={handleTextChange}/>
-    <textField hint="" bind:text="{_longitude}" on:textChange={handleTextChange}/>
-    <textField hint="" bind:text="{_altitude}" on:textChange={handleTextChange}/>
-    <textField hint="" bind:text="{_speed}" on:textChange={handleTextChange}/>
+    <textField hint="" bind:text={_latitude} on:textChange={handleTextChange} />
+    <textField
+      hint=""
+      bind:text={_longitude}
+      on:textChange={handleTextChange}
+    />
+    <textField hint="" bind:text={_altitude} on:textChange={handleTextChange} />
+    <textField hint="" bind:text={_speed} on:textChange={handleTextChange} />
     <button
       text="Aceptar"
-      on:tap={() => {
-        alert(`Your date is`).then(() => {
-          console.log("Alert dialog closed.");
-        });
+      on:tap={async () => {
+        await alert(`Your date is`);
+        console.log("Alert dialog closed.");
+        data = await getItems();
+        data=[...data.map(d=>d.sede)]
+        console.log(data);
       }}
     />
     <label class="info">
@@ -79,6 +102,7 @@
         <span text=" Hola {message}" />
       </formattedString>
     </label>
+    <listView items={data} />
   </flexboxLayout>
 </page>
 
