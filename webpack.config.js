bd7462be8ae2e2106b5d60c7b1855546cc0f00c5
ceslep <ceslep@gmail.com>
2023-07-25 07:59:21 -0500

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const webpack = require("@nativescript/webpack");
 
-module.exports = (env) => {
+module.exports = (env,options={}) => {
 	webpack.init(env);
 
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
-	webpack.chainWebpack(config => {
-		config.resolve.conditionNames=['svelte']
-	});
+	webpack.resolveConfig().resolve.conditionNames = ['svelte'];
+	const mode = options.mode || 'development';
+	webpack.resolveConfig().mode = mode;
+
+	
 	return webpack.resolveConfig();
 };
